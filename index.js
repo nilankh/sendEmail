2393a230fc95a5158132d0d2b16557e41aba6199
nilank nikhil <nilanknikhil@gmail.com>
2022-04-27 10:38:50 +0530

    Rendered html files

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express();
 // The port we want our project to run on
 const PORT = 3000;
 
+// Express should add our path
+app.use(express.static("public"));
+
+// Render the index.html when the user visit our project port
+app.get('/', (req, res) => {
+    res.sendFile("/index.html")
+})
+
 
 // Express allows us to listen to the PORT and trigger a console.log() when you visit the port
 app.listen(PORT, () => {
